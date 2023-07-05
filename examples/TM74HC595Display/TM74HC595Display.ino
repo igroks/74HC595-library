332c9da2ea82ec188f342e352cbed21eba25fd1d
Igor Carvalho <igor.carvalho@icomp.ufam.edu.br>
2023-07-05 18:10:40 -0400

    feat: add example to new functions

--- examples/TM74HC595Display/TM74HC595Display.ino
+++ examples/TM74HC595Display/TM74HC595Display.ino
@@ -1,14 +1,19 @@
 #include <TM74HC595Display.h>
 #include <TimerOne.h>
 
-int SCLK = 7;
-int RCLK = 6;
-int DIO = 5;
+//Pinos de conexao ao modulo
+int PINO_SCLK = 7;
+int PINO_RCLK = 6;
+int PINO_DIO = 5;
 
-TM74HC595Display disp(SCLK, RCLK, DIO);
-unsigned char LED_0F[29];
+int i = 0, j = 1, k = 2, l = 3;
+
+TM74HC595Display disp(PINO_SCLK, PINO_RCLK, PINO_DIO);
+unsigned char LED_0F[16];
+
 
 void setup() {
+  //Definicao dos digitos de 0 a 9
   LED_0F[0] = 0xC0; //0
   LED_0F[1] = 0xF9; //1
   LED_0F[2] = 0xA4; //2
@@ -25,73 +30,44 @@ void setup() {
   LED_0F[13] = 0xA1; //d
   LED_0F[14] = 0x86; //E
   LED_0F[15] = 0x8E; //F
-  LED_0F[16] = 0xC2; //G
-  LED_0F[17] = 0x89; //H
-  LED_0F[18] = 0xF9; //I
-  LED_0F[19] = 0xF1; //J
-  LED_0F[20] = 0xC3; //L
-  LED_0F[21] = 0xA9; //n
-  LED_0F[22] = 0xC0; //O
-  LED_0F[23] = 0x8C; //P
-  LED_0F[24] = 0x98; //q
-  LED_0F[25] = 0x92; //S
-  LED_0F[26] = 0xC1; //U
-  LED_0F[27] = 0x91; //Y
-  LED_0F[28] = 0xFE; //hight -
-
   
+
   Timer1.initialize(1500); // set a timer of length 1500
   Timer1.attachInterrupt(timerIsr); // attach the service routine here
 
+  //Testes iniciais
+  //Envia o numero 1 para o primeiro digito
+  disp.set(LED_0F[1], 3);
+  delay(1000);
+  //Apaga o display
+  disp.clearAll();
+
+  //Envia o numero 2 para o primeiro digito
+  disp.set(LED_0F[2], 2);
+  delay(1000);
+  disp.clearAll();
+
+  //Envia o numero 3 para o primeiro digito
+  disp.set(LED_0F[3], 1);
+  delay(1000);
+  disp.clearAll();
+
+  //Envia o numero 4 para o primeiro digito
+  disp.set(LED_0F[4], 0);
+  delay(1000);
+  disp.clearAll();
 }
 
-void loop() {  
-
-  disp.set(LED_0F[0], 0);    //send digital "0" to 1st indicator
+void loop(){
+  //Mostra o valor do contador no display
+  Serial.println(i);
+  disp.digit(3, LED_0F[i++%16]);
+  disp.digit(2, LED_0F[j++%16]);
+  disp.digit(1, LED_0F[k++%16]);
+  disp.digit(0, LED_0F[l++%16]);
   delay(1000);
-  disp.set(LED_0F[3], 1);    //send digital "3" to 2nd indicator
-  delay(1000);
-  disp.set(LED_0F[10], 2);    //send simbol "A" to 3rd indicators
-  delay(1000);
-  disp.set(LED_0F[11], 3);    //send simbol "b" to 4rd indicators
-  delay(1000);
-
-
-  disp.clear();   //clear display
-
-
-  for(int i = 0; i<=99; i++){
-  disp.digit2(i, 0);               //send counter 0-99 in 1st and 2nd view ports
-  delay(50);
-  }
-
-   disp.clear();
-
-  for(int i = 0; i<=99; i++){
-  disp.digit2(i, 1);               //send counter 0-99 in 3rd and 4rd view ports
-  delay(50);
-  }
-
-  disp.clear();
-
-  for(int i = 0; i<=100; i++){
-  disp.digit4showZero(i);               //send counter 0-100 with zero
-  delay(50);
-  }
-  disp.clear();
-
-  for(int i = 0; i<=9999; i++){
-  disp.digit4(i);               //send counter 0-9999 hide zero
-  delay(10);
-  }
-  disp.clear();
-
 }
 
-
-void timerIsr()
-{
+void timerIsr() {
   disp.timerIsr();
 }
-
-
